26477c25a85e08cd81126751afe321e50a7aaa35
Maciej <maciej.wozniczka@outlook.com>
2026-07-19 14:42:16 +0200

    Added Ollama and WebUI to Host project

--- AiChatBuddy.AppHost/AppHost.cs
+++ AiChatBuddy.AppHost/AppHost.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ollama =builder.AddOllama("ollama").WithDataVolume();
+ollama.AddModel("chat", "llama3.2");
+
+builder.AddContainer("open-webui", "ghcr.io/open-webui/open-webui", "main")
+    .WithHttpEndpoint(port: 3000, targetPort:8080, name: "http")
+    .WithEnvironment("Ollama_Base_Url", ollama.GetEndpoint("http"))
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WaitFor(ollama);
+
+builder.AddProject<Projects.AiChatBuddy_Api>("aichatbuddy-api")
+    .WithUrlForEndpoint("https", url => url.Url = "/swagger")
+    .WithUrlForEndpoint("http", url => url.Url = "/swagger");
+
 builder.Build().Run();
