a486c029f9d5864fa9c7580ee482347f802272a0
Maciej <maciej.wozniczka@outlook.com>
2026-08-04 11:25:49 +0200

    Added VectorChunk model

---
+++ AiChatBuddy.Api/Models/VectorChunk.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.VectorData;
+
+namespace AiChatBuddy.Api.Models;
+
+public class VectorChunk
+{
+    public const int VectorDimension = 384;
+    public const string VectorDistanceFunction = DistanceFunction.CosineDistance;
+    [VectorStoreKey]
+    public required Guid Key { get; set; }
+    [VectorStoreData]
+    public required string Content { get; set; }
+    [VectorStoreData]
+    public string? Context { get; set; }
+    [VectorStoreData]
+    public required string DocumentId { get; set; }
+    [VectorStoreVector(VectorDimension, DistanceFunction = VectorDistanceFunction)]
+    public string Embedding { get; }
+}
