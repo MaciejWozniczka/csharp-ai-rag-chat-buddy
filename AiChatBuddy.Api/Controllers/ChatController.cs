2dd34412a834a666a96466ee9ecd6101fe90399d
Maciej <maciej.wozniczka@outlook.com>
2026-08-04 11:26:34 +0200

    Updated ChatController to use vector store

--- AiChatBuddy.Api/Controllers/ChatController.cs
+++ AiChatBuddy.Api/Controllers/ChatController.cs
@@ -1,13 +1,24 @@
-﻿using AiChatBuddy.Api.Models;
+﻿using System.ComponentModel;
+using AiChatBuddy.Api.Models;
 using Microsoft.Extensions.AI;
+using Microsoft.Extensions.VectorData;
 using ChatResponse = AiChatBuddy.Api.Models.ChatResponse;
 
 namespace AiChatBuddy.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ChatController(IChatClient chatClient) : ControllerBase
+public class ChatController : ControllerBase
 {
+    private readonly IChatClient _chatClient;
+    private readonly VectorStoreCollection<string, VectorChunk> _vectorCollection;
+    private readonly ChatOptions _chatOptions = new();
+    public ChatController(IChatClient chatClient, VectorStoreCollection<string, VectorChunk> vectorCollection)
+    {
+        _chatClient = chatClient;
+        _vectorCollection = vectorCollection;
+        _chatOptions.Tools = [AIFunctionFactory.Create(SearchItemAsync)];
+    }
     [HttpPost]
     public async Task<ChatResponse> SendQuery([FromBody] ChatRequest request)
     {
@@ -17,12 +28,23 @@ public class ChatController(IChatClient chatClient) : ControllerBase
             new ChatMessage(ChatRole.User, request.Query)
         };
 
-        var response = await chatClient.GetResponseAsync(messages, new ChatOptions());
+        var response = await _chatClient.GetResponseAsync(messages, new ChatOptions());
 
         return new ChatResponse
         {
             Message = response.Text,
             Status = "Success"
         };
+    }
+
+    [Description("Searches for items based on the provided query.")]
+    private async Task<IEnumerable<string>> SearchItemAsync([Description("The query to search for.")] string searchQuery, CancellationToken cancellationToken)
+    {
+        var searchResults = _vectorCollection
+            .SearchAsync(searchQuery, top: 5, cancellationToken: cancellationToken);
+
+        return await searchResults
+            .Select(r => r.Record.Content)
+            .ToListAsync(cancellationToken);
     }
 }
